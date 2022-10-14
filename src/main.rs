586eb31df337a9d43a62b8bde9e051bc1ccc729b
miguelToscano <miguetoscano288@gmail.com>
2022-10-14 06:31:13 -0300

    added database dockerization script

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use newsletter::server;
+
 #[tokio::main]
 pub async fn main() -> std::io::Result<()> {
     let listener = std::net::TcpListener::bind("127.0.0.1:8080").expect("Failed to bind 8080 port");
-    newsletter::server::app::run(listener)?.await
+    server::app::run(listener)?.await
 }
