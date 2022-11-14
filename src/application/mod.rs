b005802658fb90f1f049f488b614ce69eaf61d1a
miguelToscano <miguetoscano288@gmail.com>
2022-11-14 08:42:22 -0300

    removed unused variable

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,1 +1,17 @@
 
+use crate::adapters::outbound::users_repositories::UsersRepository;
+use crate::domain::users::services as users_service;
+use crate::domain::users::types::{CreateUserInput, User};
+
+pub async fn get_users(users_repository: &impl UsersRepository) -> Result<Vec<User>, ()> {
+    let users = users_service::get_all(users_repository).await?;
+    return Ok(users);
+}
+
+pub async fn create_user(
+    users_repository: &impl UsersRepository,
+    user: &CreateUserInput,
+) -> Result<String, ()> {
+    users_service::create(users_repository, user).await?;
+    return Ok(String::from("Computed token"));
+}
