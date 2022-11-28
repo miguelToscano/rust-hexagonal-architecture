c0e2290a368bb4be14929b51550da676f2a58905
miguelToscano <miguetoscano288@gmail.com>
2022-11-28 01:01:42 -0300

    renamed env var

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use dotenv::dotenv;
 pub async fn main() -> Result<(), std::io::Error> {
     dotenv().ok();
 
-    let mode = std::env::var("MODE").unwrap_or("REST_SERVER".to_string());
+    let run_mode = std::env::var("RUN_MODE").unwrap_or("REST_SERVER".to_string());
 
-    match mode.as_str() {
+    match run_mode.as_str() {
         "REST_SERVER" => adapters::inbound::rest_server::run().await,
         "GRAPHQL_SERVER" => panic!("GraphQL Server not implemented"),
         "CLI" => panic!("CLI mode not implemented"),
-        _ => panic!("Invalid MODE env var"),
+        _ => panic!("Invalid RUN_MODE env var"),
     }
 }
