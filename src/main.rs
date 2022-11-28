94c2d504ca560bc2fa9cf3602c2aea026230751d
miguelToscano <miguetoscano288@gmail.com>
2022-11-28 00:52:10 -0300

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,12 @@ use dotenv::dotenv;
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
     dotenv().ok();
-    match std::env::var("MODE")  {
-        Ok(mode) => {
-            match mode.as_str() {
-                "REST_SERVER" => adapters::inbound::rest_server::run().await,
-                "GRAPHQL_SERVER" => panic!("GraphQL Server not implemented"),
-                "CLI" => panic!("CLI mode not implemented"),
-                _ => panic!("could not read MODE env var"),
-            }
+    match std::env::var("MODE") {
+        Ok(mode) => match mode.as_str() {
+            "REST_SERVER" => adapters::inbound::rest_server::run().await,
+            "GRAPHQL_SERVER" => panic!("GraphQL Server not implemented"),
+            "CLI" => panic!("CLI mode not implemented"),
+            _ => panic!("could not read MODE env var"),
         },
         _ => Ok(()),
     }
